6971d34e61ab0e177e8999e76b00e78e082c2ad6
abtaaahi <abtahimahib@gmail.com>
2024-07-09 15:16:12 +0600

    initial

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function checkScreenSize() {
-      if (window.innerWidth < 768) {
-        document.getElementById('mobile-message').style.display = 'flex';
-        document.body.style.overflow = 'hidden';
-      } else {
-        document.getElementById('mobile-message').style.display = 'none';
-        document.body.style.overflow = 'auto';
-      }
-    }
-    checkScreenSize();
-    window.addEventListener('resize', checkScreenSize);
-  });
-
-
 document.addEventListener("scroll", function() {
   const scrollToTopButton = document.querySelector(".scroll-to-top");
   if (window.scrollY > 200) {
